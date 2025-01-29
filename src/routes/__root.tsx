c08d351c57a21d40a639349dbb0512d77f4a73cd
Yncy <paalayancy@gmail.com>
2025-01-29 13:53:55 +0800

    thanks radix

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import HeaderComponent from "../components/Header";
 import Navbar from "../components/Navbar";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { Theme } from "@radix-ui/themes";
 //import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 //TODO: Navbar here
@@ -9,16 +10,18 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <QueryClientProvider client={new QueryClient()}>
-        <div className="flex flex-row">
-          <Navbar />
-          <div className="body-container ">
-            <main className="flex flex-col gap-4 w-full">
-              <HeaderComponent />
-              <Outlet />
-            </main>
+        <Theme>
+          <div className="flex flex-row">
+            <Navbar />
+            <div className="body-container ">
+              <main className="flex flex-col gap-4 w-full">
+                <HeaderComponent />
+                <Outlet />
+              </main>
+            </div>
           </div>
-        </div>
-        {/* <TanStackRouterDevtools /> */}
+          {/* <TanStackRouterDevtools /> */}
+        </Theme>
       </QueryClientProvider>
     </>
   ),
--- src/routes/booked_rooms.lazy.tsx
+++ src/routes/booked_rooms.lazy.tsx
@@ -3,6 +3,9 @@ import { createLazyFileRoute } from "@tanstack/react-router";
 import { fetchBookedRooms } from "@/hooks/queries/booking/useFetchBookedRooms";
 import dayjs from "dayjs";
 
+import * as Dialog from "@radix-ui/themes";
+import { Table } from "@radix-ui/themes";
+
 export const Route = createLazyFileRoute("/booked_rooms")({
   component: BookedRoomss,
 });
@@ -29,39 +32,40 @@ function BookedRoomss() {
   };
 
   return (
-    <div className="flex min-w-full">
-      <table className="min-w-full">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Time In</th>
-            <th>Time Out</th>
-            <th>Username</th>
-            <th>Room Name</th>
-            <th>Course & Section</th>
-            <th>Subject Code</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
+    <div className="min-w-full gap-7 flex flex-col">
+      <h1 className="text-center">Booked Rooms List</h1>
+      <Table.Root>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Date</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Time In</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Time Out</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Username</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Room Name</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Course & Section</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Subject Code</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
           {data?.map((booking, index) => (
-            <tr key={index}>
-              <td>{booking.date}</td>
-              <td>{dayjs(booking.time_in).format("HH:mm:ss")}</td>
-              <td>{dayjs(booking.time_out).format("HH:mm:ss")}</td>
-              <td>{booking.profiles?.username}</td>
-              <td>{booking.rooms?.room_name}</td>
-              <td>{booking.course_and_section}</td>
-              <td>{booking.subject_code}</td>
-              <td>
-                <button onClick={() => handleStatusClick(booking)}>
-                  {booking.status}
-                </button>
-              </td>
-            </tr>
+            <Table.Row key={index}>
+              <Table.Cell>{booking.date}</Table.Cell>
+              <Table.Cell>
+                {dayjs(booking.time_in).format("HH:mm:ss")}
+              </Table.Cell>
+              <Table.Cell>
+                {dayjs(booking.time_out).format("HH:mm:ss")}
+              </Table.Cell>
+              <Table.Cell>{booking.profiles?.username}</Table.Cell>
+              <Table.Cell>{booking.rooms?.room_name}</Table.Cell>
+              <Table.Cell>{booking.course_and_section}</Table.Cell>
+              <Table.Cell>{booking.subject_code}</Table.Cell>
+              <Table.Cell></Table.Cell>
+            </Table.Row>
           ))}
-        </tbody>
-      </table>
+        </Table.Body>
+      </Table.Root>
     </div>
   );
 }
