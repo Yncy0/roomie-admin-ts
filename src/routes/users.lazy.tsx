86ab7dfa911346064163c79307130af6817b6076
Yncy <paalayancy@gmail.com>
2025-01-29 15:54:42 +0800

    added user tables

--- src/routes/users.lazy.tsx
+++ src/routes/users.lazy.tsx
@@ -1,9 +1,57 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { createLazyFileRoute, useNavigate } from "@tanstack/react-router";
+import {
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import {
+  LucideFilter,
+  ChevronsLeftIcon,
+  ChevronLeft,
+  ChevronRight,
+  ChevronsRight,
+} from "lucide-react";
+import { Table } from "@radix-ui/themes";
+import React from "react";
+import { fetchProfiles } from "@/hooks/queries/profiles/useFetchProfiles";
 
 export const Route = createLazyFileRoute("/users")({
   component: Users,
 });
 
 function Users() {
-  return <div>Hello "/users"!</div>;
+  const { data, error } = fetchProfiles();
+
+  return (
+    <div className="min-w-full gap-7 flex flex-col">
+      <h1 className="text-center">Users List</h1>
+      <Table.Root>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Avatar</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Username</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Mobile Number</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Email</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>User Role</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>User Department</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {data?.map((items, index) => (
+            <Table.Row key={index}>
+              <Table.Cell>{items.avatar_url}</Table.Cell>
+              <Table.Cell>{items.username}</Table.Cell>
+              <Table.Cell>{items.mobile_number}</Table.Cell>
+              <Table.Cell>{items.email}</Table.Cell>
+              <Table.Cell>{items.user_role}</Table.Cell>
+              <Table.Cell>{items.user_department}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </div>
+  );
 }
