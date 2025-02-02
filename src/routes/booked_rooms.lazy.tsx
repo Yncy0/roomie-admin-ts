5ad4251b502b190d51b8781c9e1dd67de7f625a0
Yncy <paalayancy@gmail.com>
2025-02-02 19:52:51 +0800

    fixes on status of booked rooms

--- src/routes/booked_rooms.lazy.tsx
+++ src/routes/booked_rooms.lazy.tsx
@@ -7,6 +7,7 @@ import { Table, Badge, Dialog } from "@radix-ui/themes";
 import StatusDialog from "@/components/dialogs/StatusDialog";
 import PaginationControls from "@/components/PaginationControls";
 import { useState } from "react";
+import useSubscriptionBookedRoom from "@/hooks/queries/booking/useSubcription";
 
 export const Route = createLazyFileRoute("/booked_rooms")({
   component: BookedRoomss,
@@ -23,14 +24,18 @@ function BookedRoomss() {
   const totalPages = Math.ceil((data?.length || 0) / pageSize);
 
   // Paginate the data
-  const paginatedData = data?.slice(currentPage * pageSize, (currentPage + 1) * pageSize) || [];
+  const paginatedData =
+    data?.slice(currentPage * pageSize, (currentPage + 1) * pageSize) || [];
 
   const handlePagination = (action: string) => {
     if (action === "first") setCurrentPage(0);
     if (action === "prev" && currentPage > 0) setCurrentPage(currentPage - 1);
-    if (action === "next" && currentPage < totalPages - 1) setCurrentPage(currentPage + 1);
+    if (action === "next" && currentPage < totalPages - 1)
+      setCurrentPage(currentPage + 1);
     if (action === "last") setCurrentPage(totalPages - 1);
   };
+
+  useSubscriptionBookedRoom();
 
   return (
     <div className="container">
@@ -38,36 +43,68 @@ function BookedRoomss() {
       <Table.Root className="table">
         <Table.Header className="table-header">
           <Table.Row className="table-row">
-            <Table.ColumnHeaderCell className="table-column-header-cell">Date</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="table-column-header-cell">Time In</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="table-column-header-cell">Time Out</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="table-column-header-cell">Username</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="table-column-header-cell">Room Name</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="table-column-header-cell">Course & Section</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="table-column-header-cell">Subject Code</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="table-column-header-cell">Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="table-column-header-cell">Edit</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="table-column-header-cell">
+              Date
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="table-column-header-cell">
+              Time In
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="table-column-header-cell">
+              Time Out
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="table-column-header-cell">
+              Username
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="table-column-header-cell">
+              Room Name
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="table-column-header-cell">
+              Course & Section
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="table-column-header-cell">
+              Subject Code
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="table-column-header-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="table-column-header-cell">
+              Edit
+            </Table.ColumnHeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
           {paginatedData.map((booking, index) => (
             <Table.Row key={index} className="table-row">
               <Table.Cell className="table-cell">{booking.date}</Table.Cell>
-              <Table.Cell className="table-cell">{dayjs(booking.time_in).format("HH:mm:ss")}</Table.Cell>
-              <Table.Cell className="table-cell">{dayjs(booking.time_out).format("HH:mm:ss")}</Table.Cell>
-              <Table.Cell className="table-cell">{booking.profiles?.username}</Table.Cell>
-              <Table.Cell className="table-cell">{booking.rooms?.room_name}</Table.Cell>
-              <Table.Cell className="table-cell">{booking.course_and_section}</Table.Cell>
-              <Table.Cell className="table-cell">{booking.subject_code}</Table.Cell>
+              <Table.Cell className="table-cell">
+                {dayjs(booking.time_in).format("HH:mm:ss")}
+              </Table.Cell>
+              <Table.Cell className="table-cell">
+                {dayjs(booking.time_out).format("HH:mm:ss")}
+              </Table.Cell>
+              <Table.Cell className="table-cell">
+                {booking.profiles?.username}
+              </Table.Cell>
+              <Table.Cell className="table-cell">
+                {booking.rooms?.room_name}
+              </Table.Cell>
+              <Table.Cell className="table-cell">
+                {booking.course_and_section}
+              </Table.Cell>
+              <Table.Cell className="table-cell">
+                {booking.subject_code}
+              </Table.Cell>
               <Table.Cell className="table-cell">
                 <div className="flex flex-row items-center gap-2">
-                  <Badge color="orange" className="status-badge">{booking.status}</Badge>
+                  <Badge color="orange" className="status-badge">
+                    {booking.status}
+                  </Badge>
                 </div>
               </Table.Cell>
               <Table.Cell className="table-cell">
-                  <Dialog.Root>
-                    <StatusDialog item={booking} />
-                  </Dialog.Root>
+                <Dialog.Root>
+                  <StatusDialog item={booking} />
+                </Dialog.Root>
               </Table.Cell>
             </Table.Row>
           ))}
@@ -75,10 +112,10 @@ function BookedRoomss() {
       </Table.Root>
 
       {/* Pagination Controls */}
-      <PaginationControls 
-        currentPage={currentPage} 
-        totalPages={totalPages} 
-        handlePagination={handlePagination} 
+      <PaginationControls
+        currentPage={currentPage}
+        totalPages={totalPages}
+        handlePagination={handlePagination}
       />
     </div>
   );
